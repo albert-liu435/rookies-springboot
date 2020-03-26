4076fe2533d01cb10fc2bb6f75310e1b178611c4
liuxili <liuxili@bestseller.com.cn>
2020-03-26 19:33:05 +0800

    init

---
+++ rookie-springboot-xml/src/main/java/com/rookie/bigdata/XmlApplication.java
@@ -0,0 +1,21 @@
+package com.rookie.bigdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName bigdataXmlApplication
+ * @Description bigdataXmlApplication
+ * @Author liuxili
+ * @Date 2020/3/26 9:25
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class XmlApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(XmlApplication.class, args);
+    }
+
+}
---
+++ rookie-springboot-xml/src/main/java/com/rookie/bigdata/config/BeanConfig.java
@@ -0,0 +1,16 @@
+package com.rookie.bigdata.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * @ClassName Config
+ * @Description Config
+ * @Author liuxili
+ * @Date 2020/3/26 9:30
+ * @Version 1.0
+ */
+@Configuration
+@ImportResource("classpath:application.xml")
+public class BeanConfig {
+}
